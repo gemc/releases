2926e73a255f283fe4c93a4dbb69195295f88a33
maureeungaro <maureeungaro@gmail.com>
2021-05-28 11:23:04 -0400

    porting gsd to 3.0

--- 3.0/sensitivity/ghitallocator.cc
+++
@@ -1,6 +0,0 @@
-#include "ghit.h"
-
-// this is put here and assigned to gemc executable
-// so xcode can see the allocator
-// it is not needed in the sconscript (not sure why yet)
-G4ThreadLocal G4Allocator<GHit>* GHitAllocator = 0;
