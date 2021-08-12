74a48ad5e15396ac57075c0584156279bd9367af
maureeungaro <maureeungaro@gmail.com>
2021-08-12 14:19:58 -0400

    moved to src

--- 3.0/gemc.cc
+++ 3.0/gemc.cc
@@ -23,52 +23,13 @@ int main(int argc, char* argv[])
 	// - used thread-locally by digitization
 	loadGPlugins(gopts, gDetectorGlobal->getSensitiveVolumes(), globalDigitization);
 	
-	// init gruns. Default run number if no configuration file specified
-	// this is the weighted runs, not g4run
-	GRuns *gruns = new GRuns(gopts, globalDigitization);
 
 	
-	// initialize gemc gui
-	if(gui) {
-		gsplash.message("Starting GUI");
-		qApp->processEvents();
 
-		// passing executable to retrieve full path
-		GemcGUI gemcGui(argv[0], gopts, gruns);
-
-		// PRAGMA TODO: use option g4view to set the position
-		gemcGui.move(10, 10);
-		gemcGui.show();
-
-		gsplash.finish(&gemcGui);
-
-
-		// initializing vis manager and qt session
-		G4VisManager *visManager = new G4VisExecutive();
-		visManager->Initialize();
-
-		// intializing G4UIQt session
-		G4UIsession *session = new G4UIQt(1, argv);
-
-		// opening the g4Display GUI
-		G4Display *g4Display = new G4Display(gopts);
-
-		// PRAGMA TODO: these two calls (and maybe others?) should be in a separate function?
-		applyInitialUIManagerCommands(gopts);
-		
-		qApp->exec();
-		delete visManager;
-		delete session;
-		delete g4Display;
-	} else {
-		applyInitialUIManagerCommands(gopts);
-		gruns->processEvents();
-	}
 	
 	// alla prossima!
 	delete g4MTRunManager;
 	delete gopts;
-	cout << GEMCLOGMSGITEM << " Simulation completed, arrivederci! " << endl << endl;
 	return 1;
 }
 
